3f8c9b852372dc67fcee203ef53dadf6a0e41a68
flodebarre <florence.debarre@normalesup.org>
2021-08-24 21:27:54 +0200

    clean and update comments

--- ecdc-vaccination/app.R
+++ ecdc-vaccination/app.R
@@ -1,52 +1,57 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-
-## Todo
+## Todo before running the app
 # Load data and format the data
 # source("getData.R")
+
+# Need to update this each time the data are downloaded again
 thedate <- "2021-08-24" # TimeStamp of the data
 
 library(shiny)
 
-# Global
+# Global stuff
 
 # Load data
 newdat <- read.csv(paste0("data/ecdc-vaccination_cumulated_", thedate, ".csv"))
 
 # Load function to plot the figure
-# (uses newdat as global variable)
 source("plotFig.R")
 
-# Get list of countries in the data
+# Get list of countries in the data (will be used in drop-down lists)
 ctrs <- aggregate(newdat$Country, by = list(ReportingCountry = newdat$ReportingCountry), FUN = unique)
 countries <- as.list(ctrs[, 1])
 names(countries) <- ctrs[, 2]
 rm(ctrs) # Clean memory
 
-# Define UI for application that draws a histogram
+
+# UI ####
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("ECDC vaccination data"),
-    
+    h1("EU vaccination: compare countries"), 
     p(em("Disclaimer: Provided as-is. \n
-The figure cannot be better than what is in the data.
+The figure cannot be better than what is in the data. Some countries are not reporting age-specific data to ECDC, and cannot therefore be shown here. 
 ")), 
-    h4("Links"),
-    p("The data come from the European Centre for Disease Prevention and Control (ECDC)"),
-    a(href = "https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv", "[ECDC dataset]"), 
-    a(href = "https://www.ecdc.europa.eu/en/publications-data/data-covid-19-vaccination-eu-eea", "[ECDC vaccination webpage]"), 
-    a(href = "https://vaccinetracker.ecdc.europa.eu/public/extensions/COVID-19/vaccine-tracker.html", "[ECDC vaccine tracker]"),
-    p("Code of this Shiny app:"),
-    a(href = "")
+    HTML("<p><i>I got the idea of drawing vaccination age-pyramids after seeing @VictimOfMaths' US-UK <a href = 'https://twitter.com/VictimOfMaths/status/1424788095485071367?s=20'>example on Twitter</a>. I interverted the positions of the different colors to put the emphasis on unvaccinated populations. </i></p>"), 
+    
+    fluidRow(
+      column(width = 6, 
+          h5("Data"),
+          HTML("<p>The data come from the European Centre for Disease Prevention and Control (ECDC). The dataset is available on the <a href = 'https://opendata.ecdc.europa.eu/covid19/vaccine_tracker/csv/data.csv'>vaccination webpage</a>, and can be visualized on the <a href = 'https://vaccinetracker.ecdc.europa.eu/public/extensions/COVID-19/vaccine-tracker.html'>Vaccine tracker</a> page. Please check their 'Notes on the data', 'Country Disclaimers' for a list of potential issues. </p>"),
+      ), 
+      column(width = 6, 
+             h5("Code"),
+             HTML("<p>The source code of this Shiny app is available <a href = 'https://github.com/flodebarre/covid_vaccination/tree/main/ecdc-vaccination'>on Github</a>. You are welcome to check it and to <a href = 'mailto:florence.debarre@normalesup.org?subject=ShinyApp'>let me know</a> if you find mistakes or know better ways of doing it. </p>")
+             )
+    ),
+    
+    fluidRow(
+        column(12, 
+               h5("Notes"), 
+               p("'1 dose' can include completed vaccination if the Janssen vaccine was used, or in countries (like France) that give only one dose to some of the people who have had Covid-19 previously."))
+    ),
+    
+    hr(),
 
+    # Parameters (grayed)
+wellPanel(
     # Countries
     fluidRow(
         column(width = 4, selectInput("c1", "Country 1:",
@@ -55,33 +60,36 @@ The figure cannot be better than what is in the data.
                                       countries, selected = "FR"))
     ),
     
-    # Graduations
+    # Graduations and scale
     fluidRow(
-        column(width = 4, selectInput("byRec", "Population Graduations", c("  5000" = 5000, " 10000" = 10000, " 25000" = 25000, " 50000" = 50000, "100000" = 100000), selected = 10000, width = '200px'), offset = 4)
+        column(width = 3, p("\n\nChoose the precision of the population size graduations:")),
+        column(width = 2, selectInput("byRec", label = "Graduations", choices = c("  5000" = 5000, " 10000" = 10000, " 25000" = 25000, " 50000" = 50000, "100000" = 100000), selected = 10000, width = '200px')),
+        column(width = 4, p("Choose whether to use the same scale for both countries, or use different ones (preferable if large population size difference):")), 
+        column(width = 3, radioButtons("sameScale", "Scale", choices = c("same" = TRUE, "different" = FALSE), inline = TRUE))
     ),
     
-    # Scale
-    fluidRow(
-        column(width = 4, radioButtons("sameScale", "Scale", choices = c("same" = TRUE, "different" = FALSE), inline = TRUE), offset = 4)
-    ),
-
     # Date
     fluidRow(
         column(width = 6, offset = 3, sliderInput(inputId = "week", label = "2021 Week number (click play to animate)", min = 1, max = as.numeric(substr(max(newdat$YearWeekISO), start = 7, stop = 8)), value = as.numeric(substr(max(newdat$YearWeekISO), start = 7, stop = 8)), step = 1, animate = animationOptions(interval = 750, playButton = "play"), width = '100%'))
     ),
+), 
     
     # Plot
-    fluidRow(column(width = 8, offset = 2, plotOutput("agePyramid"))),
+    fluidRow(column(width = 10, offset = 1, plotOutput("agePyramid", height = 500))),
     
+    # Trick to add white space at the bottom (not sure it is really working!)
+    p("  "),
+    fluidRow(column(1))
 )
 
-# Define server logic required to draw a histogram
+
+# Server ####
 server <- function(input, output) {
     
     output$agePyramid <- renderPlot({
         plotFig(as.character(input$c1), as.character(input$c2), as.character(input$week), as.logical(input$sameScale), as.numeric(input$byRec), thedate, newdat)
-    })
+    }, res = 80)
 }
 
-# Run the application 
+# Run the application ####
 shinyApp(ui = ui, server = server)
