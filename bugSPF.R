c79c80b742b40540eb0a259d507dce2a4ac90aaf
flodebarre <florence.debarre@normalesup.org>
2021-10-10 00:20:48 +0200

    pdf output of the figures

--- bugSPF.R
+++ bugSPF.R
@@ -64,8 +64,8 @@ names(ageClasses) <- c("Tous âges", "0-4 ans", "5-9 ans", "10-11 ans", "12-17 a
 ageClasses
 
 # Plotting specifications for the datasets
-ltys <- c(1, 1, 2, 2, 2)
-lwds <- c(1, 1, 1.5, 1.25, 2)
+ltys <- c(1, 1, 2, 2, 4)
+lwds <- 2*c(1, 1, 1.5, 1.25, 2)
 cols <- c(gray(0), "orchid3", "seagreen3", "lightskyblue3", "orange2", "khaki2")
 
 #### PLOT ####
@@ -74,13 +74,15 @@ par(las = 1)
 
 # layout(matrix(1:16, ncol = 4, byrow = TRUE))
 layout(1)
-
+sc <- 0.75 # scale of pdf
 for(i in seq_along(ageClasses)){
-  par(mar = rep(2.5, 4) + c(2.5, 0, 0, 0))
+  pdf(width = sc*7, height = sc*8, file = paste0("pics/bugSPF_", ageClasses[i], ".pdf"))
+  par(mar = rep(2.5, 4) + c(2.5, 0, 0, -2.25))
   par(mgp = c(1.25, 0.25, 0), tck = -.005)
+  par(las = 1)
   # Initialize the plot
 plot(as.Date(data0$jour), data0$couv_complet, type = "n", ylim = c(0, 100), 
-     xlab = "jour", ylab = "Taux vaccination complète", frame.plot = FALSE, 
+     xlab = "Jour", ylab = "Taux vaccination complète", frame.plot = FALSE, 
      xaxs = "i", yaxs = "i")
 
   ag <- ageClasses[i]
@@ -106,13 +108,17 @@ plot(as.Date(data0$jour), data0$couv_complet, type = "n", ylim = c(0, 100),
   
   legend("topleft", col = cols, lty = ltys, lwd = lwds, legend = c("2021-10-09", "2021-09-27", "2021-09-24", "2021-09-16", "2021-09-01"), bty = "n", title = "Date du jeu de données")
   
-  mtext("@flodebarre | 
-Données SPF, sur 
+  mtext("@flodebarre | Code : https://github.com/flodebarre/covid_vaccination/blob/main/bugSPF.R
+Données SPF, téléchargées sur 
 https://www.data.gouv.fr/en/datasets/donnees-relatives-aux-personnes-vaccinees-contre-la-covid-19-1/ 
-pour 2021-10-09, et pour les autres sur 
-https://github.com/rozierguillaume/covid-19, data/france/donnees-vacsi-a-fra.csv
-", side = 1, line = 4.25, col = gray(0.6), cex = 0.5, font = 1, family = "mono", adj = 0)
+pour 2021-10-09, et pour les autres sur l'historique de 
+https://github.com/rozierguillaume/covid-19/blob/master/data/france/donnees-vacsi-a-fra.csv
+", side = 1, line = 4.35, col = gray(0.6), cex = 0.45, font = 1, family = "mono", adj = 0)
+  
+  dev.off()
 }
 
-
+for(ag in ageClasses){
+  system(paste0("convert -density 192 pics/bugSPF_", ag, ".pdf -quality 100 -alpha remove pics/bugSPF_", ag, ".png"))
+}
 
